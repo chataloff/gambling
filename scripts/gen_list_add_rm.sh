7a6d6f98e304ebeffb34b7bcff557fc6c2f1cba0
Pegas <root@pegas>
2024-05-09 12:47:24 +0300

    fix

---
+++ scripts/gen_list_add_rm.sh
@@ -0,0 +1,100 @@
+#!/bin/sh
+
+# ANSI color codes
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# Function to show progress during git pull
+git_pull_with_progress() {
+    git pull "$@" | while read -r line; do
+        echo "$line"
+        echo "Pulling..."
+    done
+}
+
+# Function to show progress during git push
+git_push_with_progress() {
+    git push "$@" | while read -r line; do
+        echo "$line"
+        echo "Pushing..."
+    done
+}
+
+# Print help message
+print_help() {
+    echo "Usage: $0 [-a] [-d] <external_file_path>"
+    echo "Options:"
+    echo "  -a       Add entries"
+    echo "  -d       Delete entries"
+    echo "  <external_file_path>  Path to the external file"
+}
+
+# Check if any command-line arguments are provided
+if [ "$#" -eq 0 ]; then
+    print_help
+    exit 0
+fi
+
+# Parse command line options
+while getopts "ad:" option; do
+    case "$option" in
+        a) action="add";;
+        d) action="delete";;
+        *) echo "${RED}Invalid option${NC}"; print_help; exit 1;;
+    esac
+done
+
+# Shift command line arguments to access remaining parameters
+shift $((OPTIND - 1))
+
+# Prompt user for external file path
+echo "Please provide the path to the external file:"
+read -r external_file_path
+
+# Check if the file exists
+if [ ! -f "$external_file_path" ]; then
+    echo "${RED}Error: External file does not exist.${NC}"
+    exit 1
+fi
+
+# Modify the content of the source file and remove empty lines
+while IFS= read -r line; do
+    if [ -n "$line" ]; then  # Skip empty lines
+        if [ "$action" = "delete" ]; then
+            # Delete entries if action is 'delete'
+            sed -i "/$line/d" list.txt
+        else
+            # Modify the line if action is 'add'
+            modified_line=$(echo "$line" | sed -e 's/.*www.//' -e 's/.*WWW.//' -e 's~http[s]*://~~g' -e 's~HTTP[S]*://~~g' -e 's/\/.*//g' -e 's/^/||/')"^"
+            
+            # Insert current date
+            current_date=$(date +"%Y-%m-%d")
+            modified_line="$modified_line  #$current_date"
+            
+            echo "$modified_line"
+            echo "$modified_line" >> list.txt
+        fi
+    fi
+done < "$external_file_path"
+
+# Remove duplicates
+awk '!seen[$0]++' "list.txt" > tmp_list.txt && mv tmp_list.txt list.txt
+
+# Start cleanup 
+# Clean up external file
+echo "Removing External file..."
+rm "$external_file_path"
+
+# Add changes to Git
+git add list.txt
+
+# Commit changes with current date as comment
+current_date=$(date +"%Y-%m-%d")
+git commit -m "Updated data as of $current_date"
+
+# Push changes to remote repository
+git_push_with_progress #origin master
+
+# Show output/progress of git push
+echo "Push completed."
+

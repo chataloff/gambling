1f2dd1e58f1bb223c826266cf3e22e1b44688f57
Pegas <root@pegas>
2024-05-14 08:18:43 +0300

    fix

--- scripts/add-list.sh
+++ scripts/add-list.sh
@@ -1,34 +1,3 @@
-#!/bin/sh
-
-# Function to show progress during git pull
-git_pull_with_progress() {
-    git pull "$@" | while read -r line; do
-        echo "$line"
-        echo "Pulling..."
-    done
-}
-
-# Function to show progress during git push
-git_push_with_progress() {
-    git push "$@" | while read -r line; do
-        echo "$line"
-        echo "Pushing..."
-    done
-}
-
-# Pull changes from the repository with progress
-git_pull_with_progress git@github.com:chataloff/gambling.git
-
-# Prompt user for external file path
-echo "Please provide the path to the external file:"
-read -r external_file_path
-
-# Check if the file exists
-if [ ! -f "$external_file_path" ]; then
-    echo "Error: External file does not exist."
-    exit 1
-fi
-
 # Remove empty lines from the source file
 grep -v '^[[:space:]]*$' "$external_file_path" > temp_source.txt
 
@@ -62,24 +31,4 @@ rm temp_source.txt
 
 # Rename temporary list file to the final destination file
 mv temp_list.txt list.txt
-# Start cleanup 
-# Clean up temporary file
-echo "Removing External file..."
-rm  "$external_file_path"
-
-# Removing duplicates
-awk '!seen[$0]++' "list.txt" > tmp_list.txt && mv tmp_list.txt list.txt
-
-# Add changes to Git
-git add list.txt
-
-# Commit changes with current date as comment
-current_date=$(date +"%Y-%m-%d")
-git commit -m "Updated data as of $current_date"
-
-# Push changes to remote repository
-git_push_with_progress #origin master
-
-# Show output/progress of git push
-echo "Push completed."
 
